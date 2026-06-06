d3325e3cddca0899261c45d15085c00cf5d1db99
Developer <developer@example.com>
2026-06-06 20:26:52 +0700

    feat: hardcode Google Maps iframe langsung di halaman

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -256,12 +256,9 @@
                 </div>
 
                 {{-- Map --}}
-                @php $mapEmbed = $sections['location']['content']['map_embed'] ?? ''; @endphp
-                @if($mapEmbed)
-                    <div class="mt-8 rounded-2xl overflow-hidden shadow-lg">
-                        <iframe src="{{ $mapEmbed }}" width="100%" height="250" style="border:0;" allowfullscreen="" loading="lazy"></iframe>
-                    </div>
-                @endif
+                <div class="mt-8 rounded-2xl overflow-hidden shadow-lg">
+                    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3960.516398897938!2d108.4452631749967!3d-6.948247393051938!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e6f17eb2ef9bec7%3A0xde92298095af4853!2sThe%20Icon%20Hotel%20Kuningan!5e0!3m2!1sid!2sid!4v1780752163179!5m2!1sid!2sid" width="100%" height="250" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+                </div>
             </div>
 
             {{-- Contact Form --}}
